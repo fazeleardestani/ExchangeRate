580f26b47fc31ee2d4d078b8b35fd207a55a8e80
FZ_ardestani <fazeleardestani@gmail.com>
2023-05-29 19:36:20 +0330

    ExchangeRate

---
+++ windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,24 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "D:\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "F:\\Flutter\\Course\\Arz\\arz" PROJECT_DIR)
+
+set(FLUTTER_VERSION "1.0.0+1" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 1 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=D:\\flutter"
+  "PROJECT_DIR=F:\\Flutter\\Course\\Arz\\arz"
+  "FLUTTER_ROOT=D:\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=F:\\Flutter\\Course\\Arz\\arz\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=F:\\Flutter\\Course\\Arz\\arz"
+  "FLUTTER_TARGET=F:\\Flutter\\Course\\Arz\\arz\\lib\\main.dart"
+  "DART_DEFINES=RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ=="
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=F:\\Flutter\\Course\\Arz\\arz\\.dart_tool\\package_config.json"
+)
